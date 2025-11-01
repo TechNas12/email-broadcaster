61ce89db58c3aeaf1a1ffa6aaa7e65590ed27014
Sanket Dahotre <dahotresanket12@gmail.com>
2025-11-01 23:05:13 +0530

    added models to mailer app

--- mailer/models.py
+++ mailer/models.py
@@ -1,7 +1,19 @@
 from django.db import models
+from django_cryptography.fields import encrypt
 
 # Create your models here.
 class Sender(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField(unique=True)
-    password = models.P
+    app_password = encrypt(models.CharField(max_length=19))
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    
+    class Meta:
+        verbose_name = "Sender Email Account"
+        verbose_name_plural = "Sender Email Accounts"
+        ordering = ["-created_at"]
+    
+    def __str__(self):
+        return f"Sender Name: {self.name} | Sender email: {self.email}"
