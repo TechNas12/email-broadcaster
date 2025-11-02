44c37f84a1295cfc0cd4a9f2b7c33b2a288c955a
Sanket Dahotre <dahotresanket12@gmail.com>
2025-11-02 13:44:02 +0530

    Trying to create login page and signup page

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,5 @@ app_name = 'app'
 
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),
-    path('login/', views.login_view, name='login'),
-    path('signup/', views.signup_view, name='signup'),
 ]
 
